401e7aa79be7bc9663740448c9d283d21d43166c
ikeigbo <bigaike@yahoo.com>
2024-09-15 23:16:20 -0400

    Second commit

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 resource "local_file" "petclinic" {
   //filename             = "${path.module}/files/outputfile"
   filename             = "C:\\Users\\ikech\\Documents\\petclinic.txt"
-  content             = "This is a test file"
+  content             = "This is a test file. Added a second test line."
 }
